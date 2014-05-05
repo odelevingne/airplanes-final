1cdf006e4b24c8686889958ac5c01bba926dc29d
Oliver Delevingne <oliver@delevingne.co.uk>
2014-05-05 13:55:28 +0100

    added contexts to rspec

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,35 +3,43 @@ require 'plane'
 describe 'plane' do
 
 	let(:plane) {Plane.new}
+
+	context 'Taking off and landing' do
 	
-	it "should be flying when created" do
-		expect(plane).to be_flying
+		it "should be flying when created" do
+			expect(plane).to be_flying
+		end
+
+		it "should have a flying status when in the air" do
+			expect(plane.flying?).to eq "Flying!"
+		end
+
+		it "should be able to land" do
+			plane.land!
+			expect(plane.flying?).to eq "In Airport"
+		end
+
+		it "should be able to take off" do
+			plane.land!.takeoff!
+			expect(plane.flying?).to eq "Flying!"
+		end
+
 	end
 
-	it "should have a flying status when in the air" do
-		expect(plane.flying?).to eq "Flying!"
-	end
+	context 'Raising errors for duplicate commands' do
 
-	it "should be able to land" do
-		plane.land!
-		expect(plane.flying?).to eq "In Airport"
-	end
+		it "should return an error if told to take off whilst in the air" do
+			plane
+			expect(plane.flying?).to eq "Flying!"
+			expect{ (plane.takeoff!)}.to raise_error(RuntimeError)
+		end
 
-	it "should be able to take off" do
-		plane.land!.takeoff!
-		expect(plane.flying?).to eq "Flying!"
-	end
+		it "should return an error if told to land whilst already in airport" do
+			plane.land!
+			expect(plane.flying?).to eq "In Airport"
+			expect{ (plane.land!)}.to raise_error {RuntimeError}
+		end
 
-	it "should return an error if told to take off whilst in the air" do
-		plane
-		expect(plane.flying?).to eq "Flying!"
-		expect{ (plane.takeoff!)}.to raise_error(RuntimeError)
-	end
-
-	it "should return an error if told to land whilst already in airport" do
-		plane.land!
-		expect(plane.flying?).to eq "In Airport"
-		expect{ (plane.land!)}.to raise_error {RuntimeError}
 	end
 
 end
