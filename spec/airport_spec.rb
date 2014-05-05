57c19be28c0d019a59439482812f37c55743dde5
Oliver Delevingne <oliver@delevingne.co.uk>
2014-05-05 14:39:59 +0100

    refactored code with let statements in airport spec file

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,22 +2,20 @@ require 'airport'
 
 describe 'airport' do
 
+	let(:airport) {Airport.new}
+	let(:plane) {double :plane}
+
 	it "should be created with no planes" do
-		airport = Airport.new
 		expect(airport.plane_count).to eq []
 	end	
 
 	it "can have a plane land" do
-		airport = Airport.new
-		plane = double :plane
 		expect(plane).to receive(:land!)
 		airport.clear_to_land(plane)
 		expect(airport.plane_count).to eq [plane]
 	end
 
 	it "can have a plane take off" do
-		airport = Airport.new
-		plane = double :plane
 		expect(plane).to receive(:land!)
 		airport.clear_to_land(plane)
 		expect(airport.plane_count).to eq [plane]
